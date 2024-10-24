e0ad5ce9c7fba24c8bcc17bc813f4611379533a5
samleticias <sammyaleticia09@gmail.com>
2024-10-23 22:18:09 -0300

    atividade extra

--- ChamadaSistema.java
+++ ChamadaSistema.java
@@ -17,22 +17,54 @@ public class ChamadaSistema {
 
         try {
             if (arquivo.createNewFile()) {
-                System.out.println("\n> arquivo " + nomeArquivo + " criado com sucesso!");
+                System.out.println("\n> arquivo '" + nomeArquivo + "' criado com sucesso!");
             } else {
                 System.out.println("\n> arquivo informado já existe!");
             }
 
-            // escreve no arquivo
-            FileWriter escritor = new FileWriter(arquivo);
-            escritor.write("Só alegria hahaha");
-            escritor.close();
-            System.out.println("\n> texto adicionado ao arquivo!\n");
+            exibirMenu(scanner, arquivo);
+
         } catch (IOException e) {
-            System.out.println("\n ocorreu um erro ao criar o arquivo ...");
+            System.out.println("\n> ocorreu um erro ao manipular o arquivo ...");
             e.printStackTrace();
         }
 
         scanner.close();
+    }
+
+    public static void exibirMenu(Scanner scanner, File arquivo) {
+        String opcao = "";
+
+        while (!opcao.equals("0")) {
+            System.out.println("\n------------ menu ------------");
+            System.out.println("1. editar conteúdo do arquivo");
+            System.out.println("2. adicionar mensagem ao arquivo");
+            System.out.println("0. sair");
+            System.out.println("------------------------------");
+            System.out.print("> opção: ");
+            opcao = scanner.nextLine();
+
+            switch (opcao) {
+                case "1":
+                    System.out.print("\n> digite a nova mensagem para editar o conteúdo do arquivo: ");
+                    String novaMensagem = scanner.nextLine();
+                    editarArquivo(arquivo, novaMensagem);
+                    break;
+                case "2":
+                    System.out.print("\n> digite a mensagem para adicionar ao arquivo: ");
+                    String mensagemAdicional = scanner.nextLine();
+                    adicionarMensagemAoArquivo(arquivo, mensagemAdicional);
+                    break;
+                case "0":
+                    limparTela();
+                    System.out.println("\n> saindo...");
+                    break;
+                default:
+                    limparTela();
+                    System.out.println("\n> opção inválida!");
+                    break;
+            }
+        }
     }
 
     public static void limparTela() {
@@ -46,6 +78,26 @@ public class ChamadaSistema {
             }
         } catch (IOException | InterruptedException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    public static void editarArquivo(File arquivo, String novaMensagem) {
+        try (FileWriter escritor = new FileWriter(arquivo)) {
+            escritor.write(novaMensagem);
+            System.out.println("\n> arquivo editado com sucesso!");
+        } catch (IOException e) {
+            System.out.println("\n> erro ao editar o arquivo ...");
+            e.printStackTrace();
+        }
+    }
+
+    public static void adicionarMensagemAoArquivo(File arquivo, String mensagem) {
+        try (FileWriter escritor = new FileWriter(arquivo, true)) {
+            escritor.write("\n" + mensagem);
+            System.out.println("\n> mensagem adicionada ao arquivo com sucesso!");
+        } catch (IOException e) {
+            System.out.println("\n> erro ao adicionar ao arquivo ...");
+            e.printStackTrace();
         }
     }
 }
